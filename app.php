c62df27ea01ec6227a5cdaa3f69afdcd0254ca19
Adrian Föder <adrian@foeder.de>
2017-05-09 19:46:58 +0200

    [TASK] Initial commit

--- app.php
+++ app.php
@@ -1,17 +1,29 @@
 <?php
 declare(strict_types = 1);
 
-require __DIR__.'/vendor/autoload.php';
-
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Console\Application;
 use LuftsportvereinBacknangHeiningen\VereinsfliegerAviaSync\ConsoleCommand\FlightsOfDayCommand;
 use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\ApiClient;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\DefaultCredentials;
 use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\RemoteAccessToken;
-use Symfony\Component\Console\Application;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\RemoteAuthenticatedAccessToken;
+
+require __DIR__.'/vendor/autoload.php';
+
+if (!getenv('APP_ENV')) {
+    (new Dotenv())->load(__DIR__.'/.env');
+}
 
 $application = new Application();
 
 $apiClient = new ApiClient();
-$accessToken = new RemoteAccessToken($apiClient);
+$accessToken =
+    new RemoteAuthenticatedAccessToken(
+        $apiClient,
+        new DefaultCredentials(getenv('VF_USERNAME'), getenv('VF_PASSWORD')),
+        new RemoteAccessToken($apiClient)
+    );
 
 $application->add(new FlightsOfDayCommand(null, $apiClient, $accessToken));
 
--- src/ConsoleCommand/FlightsOfDayCommand.php
+++ src/ConsoleCommand/FlightsOfDayCommand.php
@@ -3,9 +3,12 @@ declare(strict_types = 1);
 
 namespace LuftsportvereinBacknangHeiningen\VereinsfliegerAviaSync\ConsoleCommand;
 
+use LuftsportvereinBacknangHeiningen\VereinsfliegerAviaSync\EdshAmeFlightDataCsvAdapter;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Application\Flight\FlightApiService;
 use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\AccessTokenInterface;
 use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\ApiClient;
 use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\AuthenticatedAccessTokenInterface;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Port\Adapter\Service\AmeAviaFlightDataCsvAdapter;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -34,13 +37,27 @@ class FlightsOfDayCommand extends Command
     {
         $this
             ->setName('flight:flights-of-day')
-            ->setDescription('Lists the flight data sets of a particular day')
+            ->setDescription('Lists the flight data sets of a particular day as CSV')
             ->addArgument('date', InputArgument::REQUIRED, 'The date in ISO-8601 format, i.e. YYYY-mm-dd');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
-        $output->writeln('Test test, token: ' . $this->accessToken);
+        $queryService =
+            new FlightApiService($this->apiClient, $this->accessToken);
+        $flightsToday =
+            $queryService
+                ->allFlightsDataOfDay(
+                    \DateTimeImmutable::createFromFormat(
+                        'Y-m-d',
+                        $input->getArgument('date')
+                    )
+                );
+        foreach ($flightsToday as $flightData) {
+            $csv =
+                new AmeAviaFlightDataCsvAdapter($flightData);
+            $output->writeln((string) $csv);
+        }
+        #$output->writeln('Test test, token: ' . $this->accessToken);
     }
 }
