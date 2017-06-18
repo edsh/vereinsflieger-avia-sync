1253e7608237183b2426bd68eded5110e9a94bd6
Adrian Föder <Adrian.Foeder@netformic.de>
2017-06-18 13:15:42 +0200

    [TASK] Finished implementing daily export

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,12 @@ declare(strict_types=1);
 
 namespace LuftsportvereinBacknangHeiningen\VereinsfliegerAviaSync\Controller;
 
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Application\Flight\FlightApiService;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\ApiClient;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Infrastructure\AuthenticatedAccessTokenInterface;
+use LuftsportvereinBacknangHeiningen\VereinsfliegerDeSdk\Port\Adapter\Service\AmeAviaFlightDataCsvAdapter;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -20,10 +26,27 @@ final class HomeController
      */
     private $templatingEngine;
 
-    public function __construct(AuthorizationCheckerInterface $authorizationChecker, EngineInterface $templatingEngine)
+    /**
+     * @var ApiClient
+     */
+    private $apiClient;
+
+    /**
+     * @var AuthenticatedAccessTokenInterface
+     */
+    private $accessToken;
+
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker,
+        EngineInterface $templatingEngine,
+        ApiClient $apiClient,
+        AuthenticatedAccessTokenInterface $accessToken
+    )
     {
         $this->authorizationChecker = $authorizationChecker;
         $this->templatingEngine = $templatingEngine;
+        $this->apiClient = $apiClient;
+        $this->accessToken = $accessToken;
     }
 
     public function indexAction(): Response
@@ -35,6 +58,38 @@ final class HomeController
         return
             new Response(
                 $this->templatingEngine->render('home/index.html.twig')
+            );
+    }
+
+    public function downloadFlightsForAmeAction(Request $request): Response
+    {
+        if (!$this->authorizationChecker->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException();
+        }
+
+        $queryService =
+            new FlightApiService($this->apiClient, $this->accessToken);
+        $flightsToday =
+            $queryService
+                ->allFlightsDataOfDay(
+                    \DateTimeImmutable::createFromFormat(
+                        'Y-m-d',
+                        $request->request->get('date')
+                    )
+                );
+
+        $responseData =
+            array_map(function ($flightData) {
+                return (string) new AmeAviaFlightDataCsvAdapter($flightData);
+            },
+            iterator_to_array($flightsToday->getIterator())
+            );
+
+        return
+            new Response(
+                implode("\n", $responseData),
+                200,
+                ['Content-Type' => 'text/csv']
             );
     }
 }
